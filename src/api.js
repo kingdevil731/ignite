a6355fcd08613c7a1a67b00035af19a2905c83a5
ruben forner <9657174+kingdevil731@users.noreply.github.com>
2021-01-03 15:58:49 +0200

    base , api

---
+++ src/api.js
@@ -0,0 +1,37 @@
+// Base Url
+const base_url = "https://api.rawg.io/api/";
+
+//Getting the month
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+};
+//Getting the date
+const getCurrentDay = () => {
+  const day = new Date().getDate();
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+};
+// current day/month/year
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+// Last Year
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+// Next Year
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+// Api Calls
+// Popular Games
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+
+export const popularGamesUrl = () => `${base_url}${popular_games}`;
